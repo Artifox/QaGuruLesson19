fc9921f796b02a7f20309335b8d62a9217e91270
Artsiom Koptsiukh <artsiom.koptsiukh@customertimes.com>
2022-11-05 13:50:42 +0400

    Initial commit

---
+++ src/test/java/tests/DemoWebShopTest.java
@@ -0,0 +1,84 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static helpers.CustomApiListener.withCustomTemplates;
+import static io.restassured.RestAssured.given;
+
+public class DemoWebShopTest extends TestBase {
+
+
+    @Test
+    @DisplayName("UI. Registration user")
+    void registrationTest() {
+        registrationPage.openPage()
+                .setGenderMale()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setMail(testData.userEmail)
+                .setPassword(testData.password)
+                .setConfirmPassword(testData.password)
+                .clickRegister()
+                .checkResult();
+    }
+
+    @Test
+    @DisplayName("API+UI. User authorization")
+    void authTest() {
+        String cookieKey = "NOPCOMMERCE.AUTH";
+        String cookieValue = given()
+                .filter(withCustomTemplates())
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .log().all()
+                .formParam("Email", testData.email)
+                .formParam("Password", testData.password)
+                .when()
+                .post("http://demowebshop.tricentis.com/login")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .cookie(cookieKey);
+
+        open("/login");
+        Cookie authCookie = new Cookie(cookieKey, cookieValue);
+        getWebDriver().manage().addCookie(authCookie);
+        open("");
+        $(".account").shouldHave(text(testData.email));
+    }
+
+    @Test
+    @DisplayName("API+UI. Update info user")
+    void updateTest() {
+        String cookieKey = "NOPCOMMERCE.AUTH";
+        String cookieValue = given()
+                .filter(withCustomTemplates())
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .log().all()
+                .formParam("Email", testData.email)
+                .formParam("Password", testData.password)
+                .when()
+                .post("http://demowebshop.tricentis.com/login")
+                .then()
+                .log().all()
+                .statusCode(302)
+                .extract()
+                .cookie(cookieKey);
+        open("");
+        Cookie authCookie = new Cookie(cookieKey, cookieValue);
+        getWebDriver().manage().addCookie(authCookie);
+        open("/customer/info");
+        $("#FirstName").setValue("TestFirstName");
+        $("#LastName").setValue("TestLastName");
+        $("[for='gender-female']").click();
+        $("[name='save-info-button']").click();
+        $("#FirstName").shouldHave(Condition.value("TestFirstName"));
+    }
+}
